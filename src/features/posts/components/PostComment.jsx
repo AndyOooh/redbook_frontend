1ba520409e5695613e81e29b2ff411b7ea9c608d
AndyOoh <andy.oeee@gmail.com>
2022-08-17 03:41:47 +0200

    before starting profile

--- src/features/posts/components/PostComment.jsx
+++ src/features/posts/components/PostComment.jsx
@@ -1,8 +1,10 @@
 import { useGetUserQuery } from 'features/users/usersApiSlice';
 import { Link } from 'react-router-dom';
 
+import './PostComment.scss';
+
 export const PostComment = ({ comment }) => {
-  console.log('comment.commentBy: øøøøøøøøøøøøøøøø', comment.commentBy);
+  console.log('comment...................', comment);
   const { data: user, isLoading, error } = useGetUserQuery(comment.commentBy);
 
   if (error) {
@@ -15,22 +17,28 @@ export const PostComment = ({ comment }) => {
     <div>Loading....</div>
   ) : (
     <div className='comment_item' key={comment.id}>
-      <img src={user?.picture} alt='' />
-      <div>
-        <Link to={`/profile/${user?._id}`} className=''>
-          <span>
-            {user?.first_name} {user?.last_name}
-          </span>
-        </Link>
-        <span>{comment.text}</span>
-      </div>
-      {comment.images > 0 && (
-        <div className='comment_images'>
-          {comment.images.map(image => (
-            <img src={image.url} alt='' key={image.id} />
-          ))}
+      <div className='comment_row'>
+        <img  src={user?.picture} alt='' className='profile_pic' />
+        <div className='comment_box'>
+          <div>
+            <Link to={`/profile/${user?._id}`} className=''>
+              <span className='user_names'>
+                {user?.first_name} {user?.last_name}
+              </span>
+            </Link>
+          </div>
+          <span className='comment'>{comment.text}</span>
+          <div className='images_row'>
+            {comment.images.length > 0 && (
+              <div className='comment_images'>
+                {comment.images.map(image => (
+                  <img src={image.url} alt='' key={image.id} />
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
